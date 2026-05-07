f75752a8a6875d699f02016529895ef7789ce0be
vnntonnio <acevalantonio@gmail.com>
2026-05-06 20:42:14 -0400

    fix: update all API URLs to use environment variables

--- src/componentes/CrudAdmin/TableDespachos.jsx
+++ src/componentes/CrudAdmin/TableDespachos.jsx
@@ -8,7 +8,7 @@ export const TableDespachos = () => {
 
   const despacho = async () => {
     await axios
-      .get("http://192.168.3.20/api/v1/despachos", {
+      .get(`${import.meta.env.VITE_API_URL_DESPACHOS}/api/v1/despachos`, {
         headers:{
               'Content-Type': 'application/json',
               'Accept': 'application/json'
@@ -19,7 +19,6 @@ export const TableDespachos = () => {
         setDespachos(response.data);
       });
   };
-  // Llamada a la función para obtener los datos cuando el componente se monta
   useEffect(() => {
     despacho();
   }, []);
@@ -50,35 +49,21 @@ export const TableDespachos = () => {
                 </tr>
               </thead>
               <tbody>
-                {despachos
-               
-                .map((despacho) => (
+                {despachos.map((despacho) => (
                   <tr key={despacho.idDespacho}>
                     <td className="pr-10 py-10 items-center">{despacho.idDespacho}</td>
-                    <td className="pr-10 py-10  items-center">
-                      {despacho.idCompra}
+                    <td className="pr-10 py-10 items-center">{despacho.idCompra}</td>
+                    <td className="pr-10 py-10 items-center">{despacho.direccionCompra}</td>
+                    <td className="pr-10 py-10 items-center">{despacho.fechaDespacho}</td>
+                    <td className="pr-10 py-10 items-center">{despacho.patenteCamion}</td>
+                    <td className="pr-10 py-10 items-center">
+                      {despacho.entregado ? "Despacho entregado" : "Despacho pendiente"}
                     </td>
-                    <td className="pr-10 py-10  items-center">
-                      {despacho.direccionCompra}
-                    </td>
-                    <td className="pr-10 py-10  items-center">
-                      {despacho.fechaDespacho}
-                    </td>
-                    <td className="pr-10 py-10  items-center">
-                      {despacho.patenteCamion}
-                    </td>
-                    <td className="pr-10 py-10  items-center">
-                      {despacho.entregado
-                        ? "Despacho entregado"
-                        : "Despacho pendiente"}
-                    </td>
-                    <td className="pr-10 py-10  items-center">
-                      {despacho.intento}
-                    </td>
+                    <td className="pr-10 py-10 items-center">{despacho.intento}</td>
                     <td>
                       <button
                         onClick={() => handleAbrirModal(despacho)}
-                        className="py-1 bg-orange-200 px-8 rounded-xl shadow-md hover:bg-orange-300/70 transition-all duration-300 "
+                        className="py-1 bg-orange-200 px-8 rounded-xl shadow-md hover:bg-orange-300/70 transition-all duration-300"
                       >
                         Cerrar despacho
                       </button>
@@ -90,17 +75,11 @@ export const TableDespachos = () => {
           </div>
         </div>
       </section>
-      <Modal
-        onClose={() => {
-          setOpenModal(false);
-        }}
-        open={openModal}
-      >
+      <Modal onClose={() => setOpenModal(false)} open={openModal}>
         {despachoSeleccionado && (
           <FormCierreDespacho
             despacho={despachoSeleccionado}
             onClose={() => {
-              //onclose es un prop que pasa funciones al modal con el form abierto, por ende al cerrarse, se ejecutan esas 2 funciones
               setOpenModal(false), despacho();
             }}
           />
